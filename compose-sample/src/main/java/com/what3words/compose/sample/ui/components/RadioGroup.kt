249d00bdfe041329e4b2c272bfce01938faf99c5
Prosper <prosper.ekwerike@what3words.com>
2022-10-20 15:40:21 +0100

    MT-455 created RadioGroup.kt

--- compose-sample/src/main/java/com/what3words/compose/sample/ui/components/RadioGroup.kt
+++ compose-sample/src/main/java/com/what3words/compose/sample/ui/components/RadioGroup.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.RadioButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,23 +19,39 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextOverflow
-import kotlin.math.max
+import com.what3words.compose.sample.R
 
-class RadioGroupState(val items: List<String>, selectedItemIndex: Int) {
-    var selected: String? by mutableStateOf(if (items.isNotEmpty()) items[selectedItemIndex] else null)
+class RadioGroupState<T>(val items: List<T>, selectedItemIndex: Int, val label: (T) -> String) {
+    var selected: T? by mutableStateOf(if (items.isNotEmpty()) items[selectedItemIndex] else null)
 }
 
 @Composable
-fun RadioGroup(
-    state: RadioGroupState,
+fun <T> RadioGroup(
+    state: RadioGroupState<T>,
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
-        state.items.forEach { item: String ->
-            RadioGroupItem(selected = item == state.selected, onClick = {
-                state.selected = item
-            }, label = item)
+        val ripple = rememberRipple()
+        val interactionSource = remember {
+            MutableInteractionSource()
+        }
+        state.items.forEach { item: T ->
+            RadioGroupItem(
+                selected = item == state.selected,
+                label = state.label(item),
+                modifier = Modifier.selectable(
+                    selected = item == state.selected,
+                    role = Role.RadioButton,
+                    interactionSource = interactionSource,
+                    indication = ripple,
+                    enabled = true,
+                    onClick = { state.selected = item }
+                ),
+                onClick = { state.selected = item }
+            )
         }
     }
 }
@@ -49,8 +68,8 @@ private fun RadioGroupItem(
             MutableInteractionSource()
         }
         RadioButton(
-            selected = selected, onClick = onClick, interactionSource = interactionSource,
-            colors = RadioButtonDefaults.colors(selectedColor = MaterialTheme.colors.primary)
+            modifier = Modifier.size(size = dimensionResource(id = R.dimen.normal_200)),
+            selected = selected, onClick = onClick, interactionSource = interactionSource
         )
         Text(
             text = label,
