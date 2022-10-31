4fa25d29d22f763044a9efe56d4302db9987f3bc
Prosper <prosper.ekwerike@what3words.com>
2022-10-31 10:43:35 +0000

    MT-461 Created W3WAutoSuggestTextFieldStyles

--- lib/src/main/java/com/what3words/components/compose/wrapper/W3WAutoSuggestTextField.kt
+++ lib/src/main/java/com/what3words/components/compose/wrapper/W3WAutoSuggestTextField.kt
@@ -3,9 +3,12 @@ package com.what3words.components.compose.wrapper
 import android.graphics.drawable.Drawable
 import androidx.appcompat.view.ContextThemeWrapper
 import android.widget.FrameLayout
+import androidx.annotation.StyleRes
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.zIndex
@@ -27,12 +30,14 @@ import com.what3words.components.picker.W3WAutoSuggestCorrectionPicker
 import com.what3words.components.text.W3WAutoSuggestEditText
 import com.what3words.javawrapper.response.APIResponse
 import com.what3words.javawrapper.response.SuggestionWithCoordinates
+import java.time.format.TextStyle
 
 /**
  * @param modifier Modifier to be applied to the W3WAutoSuggestEditText.
  * @param state the state object to be used to set-up the W3WAutoSuggestEditText.
  * @param ref Represents the [ConstrainedLayoutReference] that was used to constrain the [W3WAutoSuggestTextField] within a [ConstraintLayout].
  * @param onSuggestionWithCoordinates will return the [SuggestionWithCoordinates] picked by the end-user, coordinates will be null if returnCoordinates = false.
+ * @param styles [W3WAutoSuggestTextFieldStyles] that will be used to resolve the styling of this W3WAutoSuggestTextField default components. See [W3WAutoSuggestTextFieldDefaults.styles].
  * @param micIcon drawable to use as Mic Icon
  * @param suggestionPicker instance of [W3WAutoSuggestPicker] to replace the default picker
  * @param errorView custom error view can be any [AppCompatTextView] or [W3WAutoSuggestErrorMessage], default view will show below [W3WAutoSuggestEditText] (this will only show end-user error friendly message or message provided on [W3WAutoSuggestEditText.errorMessage])
@@ -46,6 +51,7 @@ fun ConstraintLayoutScope.W3WAutoSuggestTextField(
     state: W3WAutoSuggestTextFieldState,
     ref: ConstrainedLayoutReference,
     onSuggestionWithCoordinates: ((SuggestionWithCoordinates?) -> Unit),
+    styles: W3WAutoSuggestTextFieldStyles = W3WAutoSuggestTextFieldDefaults.styles(),
     micIcon: Drawable? = null,
     suggestionPicker: com.what3words.components.picker.W3WAutoSuggestPicker? = null,
     errorView: AppCompatTextView? = null,
@@ -151,4 +157,56 @@ fun ConstraintLayoutScope.W3WAutoSuggestTextField(
     } else {
         state.defaultInvalidAddressMessageView = invalidAddressMessageView
     }
+}
+
+object W3WAutoSuggestTextFieldDefaults {
+
+    fun styles(
+        @StyleRes autoSuggestEditTextStyle: Int = R.style.W3WAutoSuggestEditTextTheme,
+        @StyleRes autoSuggestPickerStyle: Int = R.style.W3WAutoSuggestPickerTheme,
+        @StyleRes autoSuggestCorrectionPickerStyle: Int = R.style.W3WAutoSuggestCorrectionPickerTheme,
+        @StyleRes autoSuggestErrorMessageStyle: Int = R.style.W3WAutoSuggestErrorMessage
+    ): W3WAutoSuggestTextFieldStyles = DefaultW3WAutoSuggestTextFieldStyles(
+        autoSuggestEditTextStyle = autoSuggestEditTextStyle,
+        autoSuggestPickerStyle = autoSuggestPickerStyle,
+        autoSuggestCorrectionPickerStyle = autoSuggestCorrectionPickerStyle,
+        autoSuggestErrorMessageStyle = autoSuggestErrorMessageStyle
+    )
+}
+
+@Immutable
+private class DefaultW3WAutoSuggestTextFieldStyles(
+    @StyleRes private val autoSuggestEditTextStyle: Int,
+    @StyleRes private val autoSuggestPickerStyle: Int,
+    @StyleRes private val autoSuggestCorrectionPickerStyle: Int,
+    @StyleRes private val autoSuggestErrorMessageStyle: Int,
+) : W3WAutoSuggestTextFieldStyles {
+    override fun autoSuggestEditTextStyle(): Int {
+        return autoSuggestEditTextStyle
+    }
+
+    override fun autoSuggestPickerStyle(): Int {
+        return autoSuggestPickerStyle
+    }
+
+    override fun autoSuggestCorrectionPickerStyle(): Int {
+        return autoSuggestCorrectionPickerStyle
+    }
+
+    override fun autoSuggestErrorMessageStyle(): Int {
+        return autoSuggestErrorMessageStyle
+    }
+}
+
+
+@Stable
+interface W3WAutoSuggestTextFieldStyles {
+
+    fun autoSuggestEditTextStyle(): Int
+
+    fun autoSuggestPickerStyle(): Int
+
+    fun autoSuggestCorrectionPickerStyle(): Int
+
+    fun autoSuggestErrorMessageStyle(): Int
 }
