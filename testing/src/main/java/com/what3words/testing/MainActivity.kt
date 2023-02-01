cc70382ba32cf81c70c14aef6cf52df34d7d8aa1
Prosper <prosper.ekwerike@what3words.com>
2023-02-01 16:31:20 +0000

    removed hardcoded api key

--- testing/src/main/java/com/what3words/testing/MainActivity.kt
+++ testing/src/main/java/com/what3words/testing/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityMainBinding
-    private var apiKey = "MOC1ZITA"//BuildConfig.W3W_API_KEY
+    private var apiKey = BuildConfig.W3W_API_KEY
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -191,7 +191,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpW3W() {
         binding.suggestionEditText.apiKey(
-            key = apiKey
+            key = apiKey,
+            endpoint = BuildConfig.W3W_PRE_PROD_URL
         )
             .onDisplaySuggestions {
             }
